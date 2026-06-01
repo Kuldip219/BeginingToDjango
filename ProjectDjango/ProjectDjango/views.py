ba66850183cb7f51a42a3c2021a4c8278e30ddc4
Siddartho Sarker <138643317+Kuldip219@users.noreply.github.com>
2026-06-01 20:46:44 +0600

    Updated views.py

--- ProjectDjango/ProjectDjango/views.py
+++ ProjectDjango/ProjectDjango/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Hello, World! This is the home page.")
 
+def about(request):
+    return HttpResponse("This is the about page. Here you can learn more about us.")
+
+def contact(request):
+    return HttpResponse("This is the contact page. Feel free to reach out to us!")
