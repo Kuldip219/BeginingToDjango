f0c5b6a26b7ee638f064e96753b9101f03fcc3db
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-06-01 21:23:25 +0600

    Updated templates file.

--- ProjectDjango/ProjectDjango/views.py
+++ ProjectDjango/ProjectDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello, World! This is the home page.")
-    return render(request, 'index.html')  
+    return render(request, 'website/index.html')  
 def about(request):
     return HttpResponse("This is the about page. Here you can learn more about us.")
 
