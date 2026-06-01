e61ef7e8e89777a49780ac05e95c93fc7dab8f79
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-06-01 21:15:52 +0600

    Enable templates and render home view
    Add 'templates' to TEMPLATES DIRS so Django will load project-level templates. Update the home view to import render and return render(request, 'index.html') instead of a plain HttpResponse (old response left commented). Auto-generated .pyc files were updated.

--- ProjectDjango/ProjectDjango/views.py
+++ ProjectDjango/ProjectDjango/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, World! This is the home page.")
-
+    # return HttpResponse("Hello, World! This is the home page.")
+    return render(request, 'index.html')  
 def about(request):
     return HttpResponse("This is the about page. Here you can learn more about us.")
 
