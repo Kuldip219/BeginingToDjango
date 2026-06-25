ae48488e953200ab7f03c85cda4995153451b268
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-06-25 11:35:34 +0600

    Add cp_type to variety and register admin
    Add a new CharField cp_type to the variety model with predefined CP_TYPE_CHOICES and default 'Type1', and implement __str__ to return the name. Create migration 0002_variety_cp_type to apply the schema change and register the variety model in admin. Also include added media images for variety reports and the updated sqlite3 database.

--- ProjectDjango/Mouse/models.py
+++ ProjectDjango/Mouse/models.py
@@ -4,6 +4,15 @@ from django.utils import timezone
 # Create your models here.
 
 class variety(models.Model):
+    CP_TYPE_CHOICES = [
+        ('Type1', 'Report Type 1'),
+        ('Type2', 'Report Type 2'),
+        ('Type3', 'Report Type 3'),
+    ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='variety_images/')
     date_created = models.DateTimeField(default=timezone.now)
+    cp_type = models.CharField(max_length=5, choices=CP_TYPE_CHOICES, default='Type1')
+
+    def __str__(self):
+        return self.name
