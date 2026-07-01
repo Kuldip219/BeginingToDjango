25eb19a722f1630c856ebe962b7b3fdc401a6aec
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-07-01 16:53:28 +0600

    Add ProjectDetail model with OneToOne relation
    Introduce ProjectDetail model that stores detail_info and date_added and links one-to-one to the existing variety model via the mice field (related_name='detail'). Adds a __str__ implementation and uses timezone.now as the default for date_added.

--- ProjectDjango/Mouse/models.py
+++ ProjectDjango/Mouse/models.py
@@ -40,3 +40,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+    
+# One to One
+
+class ProjectDetail(models.Model):
+    mice = models.OneToOneField(variety, on_delete=models.CASCADE, related_name='detail')
+    detail_info = models.CharField(max_length=100)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Detail for {self.mice.name}'
