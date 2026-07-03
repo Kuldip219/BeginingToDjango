e791037a6e04b612b8c3993f0f7197af0534d013
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-07-03 18:50:23 +0600

    Enhance Mouse admin (inlines & displays)
    Add customized admin classes for the Mouse app: register variety with varietyadmin (shows name, description, date_created and inlines ProjectReview), register ProjectDetail with projectDetailadmin (list_display and filter_horizontal on 'mice'), and register Project with projectadmin (list_display). These changes improve admin UX and editing of related models.

--- ProjectDjango/Mouse/admin.py
+++ ProjectDjango/Mouse/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import variety
+from .models import variety, ProjectReview, Project, ProjectDetail
 
 # Register your models here.
-admin.site.register(variety)
+class ProjectReviewInLine(admin.TabularInline):
+    model = ProjectReview
+    extra = 2
+
+class varietyadmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'date_created']
+    inlines = [ProjectReviewInLine]
+
+class projectDetailadmin(admin.ModelAdmin):
+    list_display = ['mice', 'detail_info', 'date_added']
+    filter_horizontal = ['mice']
+
+class projectadmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+admin.site.register(variety, varietyadmin)
+admin.site.register(ProjectDetail, projectDetailadmin)
+admin.site.register(Project, projectadmin)
